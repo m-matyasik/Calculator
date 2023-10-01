a5c28a43254b8e919329662ee7a55ecc85abb327
mariu <mariu@DESKTOP-6B3S7UU>
2023-10-01 23:47:53 +0200

    Dodaj pliki projektów.

---
+++ Calculator/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Calculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Calculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        public IActionResult Calculator(CalculatorModel calc, string calculate)
+        {
+            if (calculate == "add") calc.result = calc.firstNumber + calc.secondNumber;
+            if (calculate == "sub") calc.result = calc.firstNumber - calc.secondNumber;
+            if (calculate == "mul") calc.result = calc.firstNumber * calc.secondNumber;
+            if (calculate == "div")
+            {
+                if (calc.secondNumber == 0)
+                {
+                    ViewBag.DivisionByZeroError = "You can't divide by zero!";
+                }
+                else calc.result = calc.firstNumber / calc.secondNumber;
+            }
+
+            ViewBag.result = $"{calc.firstNumber} + {calc.secondNumber} = {calc.result}";
+
+            return View(calc);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Calculator/Models/CalculatorModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator.Models
+{
+    public class CalculatorModel
+    {
+        public int firstNumber { get;set; }
+        public int secondNumber { get;set; }
+        public int result { get;set; }
+        public char sign { get;set; }
+    }
+}
